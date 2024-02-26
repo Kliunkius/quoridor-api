a8f903cb638efda417c863b65f50708658b90b24
Aistis Butvilas <aistis.butvilas@gmail.com>
2024-02-26 23:37:50 +0200

    add database configuration

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import 'dotenv/config';
 const app = express();
 const port = process.env.PORT || 3005;
 
-// Define a route
 app.get('/', (req: Request, res: Response) => {
   res.send('Gyat damn!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
