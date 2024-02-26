186f79b3a29eaca3c8cf53623848b7a19ab9b4de
Aistis <aistis.butvilas@gmail.com>
2024-02-26 14:41:29 +0200

    configure project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3005;
+
+// Define a route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Gyat damn!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
