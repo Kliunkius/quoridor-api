43d6763685773417fcd07522ce4864737a451d96
Aistis Butvilas <aistis.butvilas@gmail.com>
2024-02-26 18:34:59 +0200

    fix import to pass lint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
-// import 'dotenv/config';
-require('dotenv').config();
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3005;
